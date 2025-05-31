3b1ae06a8d6c099d7b0bfb1d8b536a4ab72d3350
HusainMahtab <mahtabh667@gmail.com>
2025-05-31 16:57:38 +0545

    multer changes

--- src/lib/multer.ts
+++ src/lib/multer.ts
@@ -4,11 +4,11 @@ import cloudinary from "./cloudinary";
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
-    folder: "fruit-image", 
+  params: async (req, file) => ({
+    folder: "fruit-image",
     allowed_formats: ["jpg", "jpeg", "png"],
     transformation: [{ width: 500, height: 500, crop: "limit" }],
-  },
+  }),
 });
 
 const upload = multer({ storage });
